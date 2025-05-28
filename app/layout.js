417d04a771e2a0333b4fd9e2f3188ad558e5be0c
ZirvaKhan <zirvakhan979@gmail.com>
2025-05-28 15:55:25 +0500

    updated

--- app/layout.js
+++ app/layout.js
@@ -21,9 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
       <html lang="en">
         <head>
           <meta
